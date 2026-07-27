3e30099e5cfb37a1e0c6b5864847e315113f0e7c
Sayantan Chandra <shonaaton@users.noreply.github.com>
2026-07-27 18:33:14 +0530

    Add custom CSS to theme and fix media loading

--- functions.php
+++ functions.php
@@ -30,6 +30,7 @@ function cd_enqueue_assets() {
         'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700;900&family=Source+Sans+3:wght@400;600;700&display=swap',
         array(), null);
     wp_enqueue_style('cd-style', get_stylesheet_uri(), array(), CD_VERSION);
+    wp_enqueue_style('cd-additional', CD_URI . '/assets/css/additional.css', array('cd-style'), CD_VERSION);
     wp_enqueue_script('cd-main', CD_URI . '/assets/js/main.js', array('jquery'), CD_VERSION, true);
     if (is_singular() && comments_open()) wp_enqueue_script('comment-reply');
     wp_localize_script('cd-main', 'cdAjax', array(
